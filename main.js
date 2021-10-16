e122a60c24bd9a52ceb91969ef58ac1276d632d4
AndreUdotai <ubongudotai@gmail.com>
2021-10-17 00:57:11 +0100

    fixed bugs, continue calculation with the result of the previous calculation after pressing the equal sign

--- main.js
+++ main.js
@@ -36,40 +36,68 @@ const calculator = {
 const display = document.getElementById('display');
 
 const populateDisplay = (input) => {
-    let { displayValue } = calculator;
+    let { displayValue, result, allowSecondOperand } = calculator;
+    // When there is no initial display - first input
     if(displayValue === '0'){
         calculator.displayValue = input;
         display.value = calculator.displayValue;
+        // After a calculation has been completed i.e. after pressing the '=' sign
+    } else if (result && allowSecondOperand == false){
+        calculator.displayValue = input;
+        display.value = calculator.displayValue;
+        calculator.firstOperand = null;
+        calculator.secondOperand = null;
+        calculator.operator = null;
+        calculator.result = null;
+        // After pressing '=' to get the result of a calculation, you can press an operator and input
+        // another operand to do another calculaton with the result, the operator and the new inputed number.
+    } else if (result && allowSecondOperand == true) {
+        calculator.firstOperand = calculator.result;
+        calculator.displayValue = input;
+        display.value = calculator.displayValue;
+        calculator.secondOperand = null;
+
     } else {
+        // Append digits as you press numbers
+     
         calculator.displayValue = displayValue + input;
         display.value = calculator.displayValue;
+
+    console.table(calculator)
     }
+
 }
 
 const handleOperator = (operatorSign) => {
-    let { displayValue, firstOperand, operator } = calculator;
-
+    let { displayValue, firstOperand, operator, result } = calculator;
+    // Store inputs into 'firstOperand' when an operator is pressed for the first time
     if(displayValue != '0' && firstOperand == null){
         calculator.firstOperand = displayValue;
         calculator.displayValue = '0';
-    } else if (firstOperand != null){
+        // When an operator is pressed after making a second input - the two entries
+        // are added back to the first operator
+    } else if (firstOperand != null && result == null){
         firstOperand = parseFloat(firstOperand);
         displayValue = parseFloat(displayValue);
 
         calculator.firstOperand = (parseFloat(operate(operator, firstOperand, displayValue).toFixed(7))).toString();
         calculator.displayValue = '0';
         display.value = calculator.firstOperand;
-    }
+    } 
     calculator.allowSecondOperand = true;
     calculator.operator = operatorSign;
+    console.table(calculator)
+
 }
 
 const handleResult = () => {
     let { allowSecondOperand, secondOperand, firstOperand, displayValue, operator, result} = calculator;
-
+    // Stores display value into 'secondOperand' variable 
     if(displayValue !== '0' && allowSecondOperand){
         calculator.secondOperand = displayValue;
         secondOperand = calculator.secondOperand;
+        // store the result in firstOperand (this covers for cases when a user continously presses
+        // the '=' sign when a calculation is completed.
     } else if (typeof(result) === 'string'){
         firstOperand = result;
     }
@@ -78,7 +106,7 @@ const handleResult = () => {
     operator = calculator.operator;
 
     calculator.result = (parseFloat(operate(operator, firstOperand, secondOperand).toFixed(7))).toString();
-    console.log(calculator.result)
+
     calculator.displayValue = calculator.result;
     display.value = calculator.displayValue;
     
@@ -95,6 +123,8 @@ const handleAllClear = () => {
     calculator.result               = null;
 
     display.value = calculator.displayValue;
+    console.table(calculator)
+
 }
 
 const handleDecimal = (dot) => {
@@ -104,6 +134,8 @@ const handleDecimal = (dot) => {
         calculator.displayValue     = displayValue + dot;
         display.value = calculator.displayValue;
     }
+    console.table(calculator)
+
 }
 
 const handleClear = () => {
@@ -115,6 +147,8 @@ const handleClear = () => {
     } else {
         display.value = '0'
     }
+    console.table(calculator)
+
 }
 
 const keys = document.getElementById('keys');
